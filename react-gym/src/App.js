8a75f2bb44d1201727bc8c275370e63983a62817
Tushar <reddygtvs@gmail.com>
2021-09-23 17:28:25 +0530

    basic page components formed

--- react-gym/src/App.js
+++ react-gym/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = "Welcome to the react Gym"
   const source = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
+        <Home />
         <p>{ Math.random() * 10 }</p>
         <a href = { source }>Source</a>
       </div>
---
+++ react-gym/src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>react-Gym</h1>
+            <div className="links">
+                <a href = "/">Home</a>
+                <a href = "/create">New Routine</a>
+                
+            </div>
+        </nav>
+      );
+}
+ 
+export default Navbar;
