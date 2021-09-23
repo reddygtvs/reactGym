0b0f093375d44666138898f3f3dcfaae0b33ac46
Tushar <reddygtvs@gmail.com>
2021-09-23 16:58:18 +0530

    basic file setup

--- react-gym/src/App.js
+++ react-gym/src/App.js
@@ -1,72 +1,14 @@
 import React from 'react';
+import './App.css';
 
-class FormData extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { weight: "" };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(e) {
-    this.setState({weight: e.target.weight});
-  }
-
-  handleSubmit(e) {
-    console.log(this.state.weight);
-    e.preventDefault();
-  }
-
-  render() {
-    return(
-      <div className="App">
-        <form onSubmit={this.handleSubmit}>
-          {/* Input for weight */}
-          <label>
-            <p>Enter your maximum weight lifted:</p>
-            <input 
-              type="number" 
-              id="weight" 
-              value={this.state.weight}
-              onChange={this.handleChange}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App Component</h1>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default FormData;
-        //Sample Change for new branch
-        // {/* Input for reps */}
-        // <label for="reps">
-        //   <p>Enter your maximum reps for that weight:</p>
-        //   <input type="number" id="reps" name="reps"></input>
-        // </label>
-
-        // {/* Input for exercise */}
-        // <label for="exercise">
-        //   <p>Choose an exercise:</p>
-        // </label>
-
-        // {/* Selection list for exercise input */}
-        // <select name="excercises" id="excercise">
-        //   <option value="Deadlift">Deadlift</option>
-        //   <option value="Bench Press">Bench Press</option>
-        //   <option value="Shoulder Press">Shoulder Press</option>
-        //   <option value="Squat">Squat</option>
-        // </select>
-
-        // {/* Submit & Reset Buttons */}
-        // <div>
-        //   <a>
-        //     <button type="submit">Get your routine</button>
-        //   </a>
-        //   <a>
-        //     <button type="reset">Reset</button>
-        //   </a>  
-        // </div>
+export default App;
--- react-gym/src/index.js
+++ react-gym/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import FormData from './App';
+import App from './App';
 import NameForm from './exp';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <FormData /> */}
-    <NameForm />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
