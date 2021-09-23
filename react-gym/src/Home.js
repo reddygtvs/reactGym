6085c3d142af372883e61851efc111caa8e15e15
Tushar <reddygtvs@gmail.com>
2021-09-23 18:13:35 +0530

    button with states

--- react-gym/src/Home.js
+++ react-gym/src/Home.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 const Home = () => {
+    //let name = 'Tushar';
+    const[name, setName] = useState('Tushar');
+    const handleClick = () => {
+        setName('Anannya');
+    }
     return (
         <div className="home">
             <h2>Welcome to your Gym Routine</h2>
-            <button>Click me</button>
+            <p>{ name }</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
       );
 }
