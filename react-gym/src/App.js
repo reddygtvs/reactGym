657ed75af30d45adfb5b4a52b039ec30490462e2
Tushar <reddygtvs@gmail.com>
2021-09-23 21:28:42 +0530

    added React Router

--- react-gym/src/App.js
+++ react-gym/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 
 function App() {
   const source = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
-        <p>{ Math.random() * 10 }</p>
-        <a href = { source }>Source</a>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+              <p>{ Math.random() * 10 }</p>
+              <a href = { source }>Source</a>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
