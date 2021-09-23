2fc8a5a1a125970d17ca39396189c298ce9880f8
Tushar <reddygtvs@gmail.com>
2021-09-23 18:40:45 +0530

    basic output format & styling

--- react-gym/src/Home.js
+++ react-gym/src/Home.js
@@ -1,16 +1,20 @@
 import { useState } from 'react';
 
 const Home = () => {
-    //let name = 'Tushar';
-    const[name, setName] = useState('Tushar');
-    const handleClick = () => {
-        setName('Anannya');
-    }
+    const [routines, setRoutines] = useState([
+        { exercise: 'Deadlift', weight: '100 kg', reps: '5', id: 1 },
+        { exercise: 'Squat', weight: '80 kg', reps: '7', id: 2 },
+        { exercise: 'Bench Press', weight: '60 kg', reps: '3', id: 1 },
+        { exercise: 'Shoulder Press', weight: '50 kg', reps: '1', id: 1 }
+    ]);
     return (
         <div className="home">
-            <h2>Welcome to your Gym Routine</h2>
-            <p>{ name }</p>
-            <button onClick={handleClick}>Click me</button>
+            {routines.map((routine) => (
+                <div className="routine-preview" key={routine.id}>
+                    <h2>{ routine.exercise }</h2>
+                    <p>Your max: { routine.weight }</p>
+                </div>
+            ))}
         </div>
       );
 }
