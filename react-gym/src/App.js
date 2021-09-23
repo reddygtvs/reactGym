f5c004727c2cd6ee9a03ca9066f7e64c81312f5a
Tushar <reddygtvs@gmail.com>
2021-09-23 17:15:00 +0530

    title and href

--- react-gym/src/App.js
+++ react-gym/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the react Gym"
+  const source = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>{ Math.random() * 10 }</p>
+        <a href = { source }>Source</a>
       </div>
     </div>
   )
