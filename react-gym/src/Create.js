9181d3de1b42f8d5fef3cafa61af5b4e6706cddb
Anannya Mahapatra <anannya1947@gmail.com>
2021-09-24 20:50:31 +0530

    form layout

--- react-gym/src/Create.js
+++ react-gym/src/Create.js
@@ -2,6 +2,22 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a New Routine</h2>
+            <form >                
+                <label>Enter your maximum weight lifted:</label>
+                <input type="number" required/>
+            
+                <label>Enter your maximum reps for that weight:</label>
+                <input type="number" required/>
+            
+                <label>Choose an excercise:</label>
+                <select>
+                    <option value="Deadlift">Deadlift</option>
+                    <option value="Bench Press">Bench Press</option>
+                    <option value="Shoulder Press">Shoulder Press</option>
+                    <option value="Squat">Squat</option>
+                </select>   
+                <button>Submit</button>                             
+            </form>
         </div>
       );
 }
