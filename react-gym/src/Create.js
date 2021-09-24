33b6f32eb93cc556fc9a13a3050d36641c19b4c5
Anannya Mahapatra <anannya1947@gmail.com>
2021-09-24 21:11:31 +0530

    accessing user input

--- react-gym/src/Create.js
+++ react-gym/src/Create.js
@@ -1,22 +1,43 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [maxWeight, setMaxWeight] = useState();
+    const [maxReps, setMaxReps] = useState();
+    const [exercise, setExercise] = useState('');
     return (
         <div className="create">
             <h2>Add a New Routine</h2>
             <form >                
                 <label>Enter your maximum weight lifted:</label>
-                <input type="number" required/>
+                <input
+                    type="number"
+                    value={maxWeight}
+                    onChange={(e) => setMaxWeight(e.target.value)}
+                    required
+                />
             
                 <label>Enter your maximum reps for that weight:</label>
-                <input type="number" required/>
+                <input
+                    type="number" 
+                    value={maxReps}
+                    onChange={(e) => setMaxReps(e.target.value)}
+                    required
+                />
             
-                <label>Choose an excercise:</label>
-                <select>
+                <label>Choose an exercise:</label>
+                <select
+                    value={exercise}
+                    onChange={(e) => setExercise(e.target.value)}
+                >
                     <option value="Deadlift">Deadlift</option>
                     <option value="Bench Press">Bench Press</option>
                     <option value="Shoulder Press">Shoulder Press</option>
                     <option value="Squat">Squat</option>
                 </select>   
-                <button>Submit</button>                             
+                <button>Submit</button> 
+                <p>{maxWeight}</p>
+                <p>{maxReps}</p>  
+                <p>{exercise}</p>                         
             </form>
         </div>
       );
