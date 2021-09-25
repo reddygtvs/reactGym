cb585485158e375b90ee562520a9069c0087ce67
Tushar <reddygtvs@gmail.com>
2021-09-25 18:31:55 +0530

    added delete function for blog

--- react-gym/src/RoutineDetails.js
+++ react-gym/src/RoutineDetails.js
@@ -1,9 +1,18 @@
 import { useParams } from "react-router-dom";
 import useFetch from './useFetch';
+import { useHistory } from 'react-router-dom';
 
 const RoutineDetails = () => {
     const { id } = useParams();
     const { data: routine, error, isPending } = useFetch('http://localhost:8000/routines/' + id);
+    const history = useHistory();
+    const handleClick = () => {
+        fetch('http://localhost:8000/routines/' + routine.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    }
     return (
         <div className="routine-details">
             { isPending && <div>Loading...</div> }
@@ -12,7 +21,9 @@ const RoutineDetails = () => {
                 <article>
                     <h2>{ routine.exercise }</h2>
                     <p>Your max is: { routine.weight }</p>
+                    
                     <div>Reps: { routine.reps }</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
             
